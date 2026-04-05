b8b9ee8b4b945b9117b271f306843392dd967fea
Bi257 <dluu5032@gamil.com>
2026-04-05 13:40:06 +0700

    fix dockerfile

--- src/main/java/com/example/demo/repository/BookingRepository.java
+++ src/main/java/com/example/demo/repository/BookingRepository.java
@@ -4,5 +4,5 @@ import com.example.demo.model.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
-    int countByRoom(String room);
+
 }
